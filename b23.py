55140ab3806d000c2f9569d783f8559716478c39
Saurabh Srivastava <drsaurabhksrivastava@gmail.com>
2024-09-10 16:13:39 +0530

    lab day1 b 87commit

---
+++ b23.py
@@ -0,0 +1,13 @@
+# a = input('Enter value of a:')
+# b = int(input('Enter value of b:'))
+# print('a is {} and b is {}'.format(a, b))
+# print(id(a))
+# print(id(b))
+# print(type(a))
+# print(type(b))
+
+
+a = int('10')
+b = int('34')
+c = a + b
+print(c)
